33d58fcb15e4d77a1480dec0928d0e1ce25c3629
peitsan <reality3iiru@163.com>
2024-05-11 11:13:26 +0800

    feat:update

--- USER/main.c
+++ USER/main.c
@@ -3,62 +3,62 @@
 #include "stdio.h"
 #include "Oled.h"
 #include "pwm.h"
-//头文件引用
+//????????
 
-#define key_set PBin(10)//设置按键
-#define key_plus PBin(1)//温湿度+
-#define key_sub PBin(0)//温湿度-
-#define motor_fan PAout(1)//散热风扇
-#define motor_heat PAout(2)//加熱片
-#define motor_hum PBout(13)//加湿电机
-#define beep PBout(12)//蜂鸣器
-#define long_press_time 500 //长按时间
-#define key_speed 50 //双击时间
-#define key_status_once 1//单击状态
-#define key_status_double 2 //双击状态
-#define key_status_long 3 //长按状态
+#define key_set PBin(10)//???????
+#define key_plus PBin(1)//?????+
+#define key_sub PBin(0)//?????-
+#define motor_fan PAout(1)//??????
+#define motor_heat PAout(2)//????
+#define motor_hum PBout(13)//??????
+#define beep PBout(12)//??????
+#define long_press_time 500 //???????
+#define key_speed 50 //??????
+#define key_status_once 1//??????
+#define key_status_double 2 //?????
+#define key_status_long 3 //??????
 
-//宏定义
+//????
 
 u8 temp = 0, hum = 0, key_status;
 	int buf1=0,buf2=19,buf3=56;
 ////////////////////////////////////////////////////////////////////////////////// 	 
-// 参数说明：
-// buf1:控制启动开关 0：锁定 1:温度  2:湿度
-// buf2:设定温度值   0.0~100.0摄氏度
-// buf3:设定湿度值   0.0%~100.0%
+// ?????????
+// buf1:???????????? 0?????? 1:???  2:???
+// buf2:??…????   0.0~100.0?????
+// buf3:??…????   0.0%~100.0%
 ////////////////////////////////////////////////////////////////////////////////// 
 	DHT11_Data_TypeDef data;
 	unsigned char buf[127] = { 0 };
-	int motor_hum_trigger = 0; //加湿模式切换
-//全局变量
+	int motor_hum_trigger = 0; //??????§??
+//??????
 
 
 void OUTPUT_Init(void)
 { 
  GPIO_InitTypeDef  GPIOA_InitStructure, GPIOB_InitStructure, GPIOH_InitStructure;
- 			 //IO输出端口配置
- GPIOA_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;				 //IO输出端口配置
- GPIOA_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //推挽输出
+ 			 //IO??????????
+ GPIOA_InitStructure.GPIO_Pin = GPIO_Pin_1 | GPIO_Pin_2;				 //IO??????????
+ GPIOA_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //???????
  GPIOA_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
- GPIOB_InitStructure.GPIO_Pin = GPIO_Pin_12| DHT11_Out_Pin |GPIO_Pin_13;				 //IO输出端口配置
- GPIOB_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //推挽输出
+ GPIOB_InitStructure.GPIO_Pin = GPIO_Pin_12| DHT11_Out_Pin |GPIO_Pin_13;				 //IO??????????
+ GPIOB_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 		 //???????
  GPIOB_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
  
  GPIO_Init(GPIOA, &GPIOA_InitStructure);
  GPIO_Init(GPIOB, &GPIOB_InitStructure);	
 
- GPIO_SetBits(GPIOA,GPIO_Pin_2);						 //输出高
- GPIO_SetBits(GPIOA,GPIO_Pin_1);						 //输出高
- GPIO_SetBits(GPIOB,GPIO_Pin_12);						 //输出高
- GPIO_ResetBits(GPIOB,GPIO_Pin_13);						 //输出低，将PB13设置为输出低电平 给加湿器提供信号
+ GPIO_SetBits(GPIOA,GPIO_Pin_2);						 //?????
+ GPIO_SetBits(GPIOA,GPIO_Pin_1);						 //?????
+ GPIO_SetBits(GPIOB,GPIO_Pin_12);						 //?????
+ GPIO_ResetBits(GPIOB,GPIO_Pin_13);						 //????????PB13???????????? ????????????
 }
 
 void KEY_Init(void)
 {
-	GPIO_InitTypeDef GPIO_InitStructure; //定义结构体变量	
+	GPIO_InitTypeDef GPIO_InitStructure; //??????????	
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
 	GPIO_InitStructure.GPIO_Pin= GPIO_Pin_0|GPIO_Pin_1|GPIO_Pin_10;
 	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_IPU;	
@@ -69,55 +69,55 @@ void KEY_Init(void)
 u8 key_setting(void)
 {
     u8 b, c;
-    if(key_set == 0) //设置按键按下
+    if(key_set == 0) //???????????
     {
-        delay_ms(20); //延时去抖动
-        if(key_set == 0) //设置按键消抖
+        delay_ms(20); //????????
+        if(key_set == 0) //???????????
         {
             c = 0;
-            while(key_set == 0) //按键被按下，开始计数
+            while(key_set == 0) //??????????????????
             {
                 c++;
                 delay_ms(10);
-                if(c >= long_press_time) //长按
+                if(c >= long_press_time) //????
                 {
                     return key_status_long;
                 }
             }
-            //按键已释放
+            //?????????
             for(b = 0; b < key_speed; b++)
             {
                 delay_ms(20);
-                if(key_set == 0) //第二次按键操作
+                if(key_set == 0) //????????????
                 {
                     delay_ms(10);
-                    if(key_set == 0) //第二次按键操作消抖
+                    if(key_set == 0) //????????????????
                     {
-                        while(key_set == 0); //等待按键释放
-                        return key_status_double; //双击
+                        while(key_set == 0); //??????????
+                        return key_status_double; //???
                     }
                 }
             }
-            //没有第二次按键操作，返回单击
+            //??§?????????????????????
             return key_status_once;
         }
     }
-    //按键没有被按下，返回0
+    //??????§???????????0
     return 0;
 }
 void hum_on(void){
-	if (motor_hum_trigger % 3 == 0) { // 七彩模式
+	if (motor_hum_trigger % 3 == 0) { // ?????
           			  motor_hum=1,delay_ms(200),motor_hum=0,delay_ms(400);motor_hum=1;
-    }else if (motor_hum_trigger % 3 == 1) { // 七彩模式
+    }else if (motor_hum_trigger % 3 == 1) { // ?????
           			  motor_hum=1;delay_ms(200),motor_hum=0;
     }	
 		motor_hum_trigger ++;
 }
 void hum_off(void){
-	if (motor_hum_trigger % 3 == 1) { // 七彩模式
+	if (motor_hum_trigger % 3 == 1) { // ?????
           			  motor_hum=1,delay_ms(200),motor_hum=0,delay_ms(400);motor_hum=1;
 					  motor_hum_trigger = 0;
-    }else if (motor_hum_trigger % 3 == 2) { // 七彩模式
+    }else if (motor_hum_trigger % 3 == 2) { // ?????
           			  motor_hum=1;delay_ms(200),motor_hum=0;
 					  motor_hum_trigger = 0;
     }else
@@ -130,9 +130,9 @@ void refresh(void){
 				hum_off();
 				motor_fan=1;
 				motor_heat=0;
-				PWM_Set_fan(data.temp_int, buf2); //开风扇
+				PWM_Set_fan(data.temp_int, buf2); //??????
 				PWM_Set_heat(0);
-				beep=1,delay_ms(50),beep=0,delay_ms(20),beep=1,delay_ms(20),beep=0;//温度过高或者湿度过低 开启蜂鸣器提醒
+				beep=1,delay_ms(50),beep=0,delay_ms(20),beep=1,delay_ms(20),beep=0;//??????????????? ??????????????
 		}
 		else if(((data.temp_int + 0.1 * data.temp_deci) < buf2) && ((data.humi_int + 0.1 * data.humi_deci) >buf3))  {	
 				beep=0;
@@ -140,14 +140,14 @@ void refresh(void){
 				motor_fan=1;
 				motor_heat=1;
 			 	PWM_Set_fan(data.humi_deci, buf3);
-			    PWM_Set_heat(1);//,开启加热电阻
+			    PWM_Set_heat(1);//,???????????
 		}
 		else if(((data.temp_int + 0.1 * data.temp_deci) < buf2) && ((data.humi_int + 0.1 * data.humi_deci) < buf3)) {
 				hum_on();
 				motor_fan=0;
 				motor_heat=1;
-				PWM_Set_fan(0, buf2);//关闭风扇
-			    PWM_Set_heat(1);//,开启加热电阻 11
+				PWM_Set_fan(0, buf2);//??????
+			    PWM_Set_heat(1);//,??????????? 11
 				beep = 0;
 		}
 		else if (((data.temp_int + 0.1 * data.temp_deci) > buf2) && ((data.humi_int + 0.1 * data.humi_deci) < buf3)){
@@ -161,19 +161,19 @@ void refresh(void){
 		else{
 				beep = 0;
 		}
-		sprintf((unsigned char *)buf, "Temp:%d.%d C", data.temp_int, data.temp_deci);//整数转字符串 温度
-		OLED_ShowString(1, 1, buf);//字符串显示
-		sprintf((unsigned char *)buf, "Humi:%d.%d %%", data.humi_int, data.humi_deci);//整数转字符串   湿度
-		OLED_ShowString(3, 1, buf);//字符串显示
-		sprintf((unsigned char *)buf, "Tset:%d C", buf2);//整数转字符串 温度
-		OLED_ShowString(2, 1, buf);//字符串显示
-		sprintf((unsigned char *)buf, "Hset:%d %%", buf3);//整数转字符串   湿度
-		OLED_ShowString(4, 1, buf);//字符串显示
+		sprintf((unsigned char *)buf, "Temp:%d.%d C", data.temp_int, data.temp_deci);//?????????? ???
+		OLED_ShowString(1, 1, buf);//????????
+		sprintf((unsigned char *)buf, "Humi:%d.%d %%", data.humi_int, data.humi_deci);//??????????   ???
+		OLED_ShowString(3, 1, buf);//????????
+		sprintf((unsigned char *)buf, "Tset:%d C", buf2);//?????????? ???
+		OLED_ShowString(2, 1, buf);//????????
+		sprintf((unsigned char *)buf, "Hset:%d %%", buf3);//??????????   ???
+		OLED_ShowString(4, 1, buf);//????????
 		
 }
 int main(void)
 {	 
-	DELAY_Init();//下面都是初始化函数
+	DELAY_Init();//???????????????
 	OLED_Init() ; 
 	PWM_Init();	
 	KEY_Init();
@@ -205,10 +205,10 @@ int main(void)
                break;
 		   default : break;
        }
-		if(key_plus==0 && buf1==1) //期望温度值+1
+		if(key_plus==0 && buf1==1) //????????+1
 		{		
 			while(key_plus!=0); 
-			{	buf2++;             //防抖
+			{	buf2++;             //????
 				beep=1; 
 			}
 			refresh();
@@ -216,10 +216,10 @@ int main(void)
 			beep=0;
 			if(buf2>100) buf2=100;	
 		}
-		if(key_sub==0&&buf1==1) //期望温度值-1
+		if(key_sub==0&&buf1==1) //????????-1
 		{		
 			while(key_sub!=0);
-			{	buf2--;             //防抖
+			{	buf2--;             //????
 				beep=1; 
 			}
 			delay_ms(30);
@@ -227,10 +227,10 @@ int main(void)
 			beep=0;
 			if(buf2<1) buf2=1;
 		}		
-		if(key_plus==0&&buf1==2) //期望湿度值+1
+		if(key_plus==0&&buf1==2) //????????+1
 		{		
 			while(key_plus!=0);
-			{	buf3++;             //防抖
+			{	buf3++;             //????
 				beep=1; 
 			}	
 			refresh();
@@ -238,10 +238,10 @@ int main(void)
 			beep=0;
 			if(buf3>100) buf3=100;	
 		}
-		if(key_sub==0&&buf1==2) //期望湿度值-1
+		if(key_sub==0&&buf1==2) //????????-1
 		{		
 			while(key_sub!=0);
-			{	buf3--;             //防抖
+			{	buf3--;             //????
 				beep=1; 
 			}
 			refresh();
@@ -249,15 +249,15 @@ int main(void)
 			beep=0;
 			if(buf3<1) buf3=1;	
 		}
-		if(buf1==1)//显示设置状态
+		if(buf1==1)//?????????
 		{
-			sprintf((char *)buf, "Tset:%d C", buf2);//整数转字符串 温度
-			OLED_ShowString(2, 1, buf);//字符串显示
+			sprintf((char *)buf, "Tset:%d C", buf2);//?????????? ???
+			OLED_ShowString(2, 1, buf);//????????
 		}
-		else if(buf1==2) //显示设置状态
+		else if(buf1==2) //?????????
 		{
-			sprintf((char *)buf, "Hset:%d %%", buf3);//整数转字符串 湿度
-			OLED_ShowString(4, 1, buf);//字符串显示
+			sprintf((char *)buf, "Hset:%d %%", buf3);//?????????? ???
+			OLED_ShowString(4, 1, buf);//????????
 		}
 	
 	}
